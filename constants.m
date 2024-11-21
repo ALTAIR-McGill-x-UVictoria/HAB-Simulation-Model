c75fdfe8df2f5ac351a847fdabdd8f3d419337d4
Eduard Anton <eduard.anton2002@gmail.com>
2024-11-20 22:23:30 -0500

    Added kinematic constraints for balloon and payload system

--- constants.m
+++ constants.m
@@ -2,12 +2,13 @@
 
 % Physical constants
 R = 8.314;      % Gas constant (J/mol*K)
-g = 9.81; % Gravitational constant on Earth (m/s^2)
+g = 9.81;       % Gravitational constant on Earth (m/s^2)
 up = [0 0 1];   % World frame up direction
 
 % Balloon variables
-h = 20000;          % Altitude (m)
-m_gas = 1600;       % Mass of gas (g)
-M_gas = 4.002602;   % Molar mass of gas (g/mol);
-m_balloon = 500;    % Mass of the balloon (g)
-m_payload = 1500;   % Mass of the payload (g)
+h = 500;                    % Initial altitude (m)
+m_gas = 1600;               % Mass of gas (g)
+M_gas = 4.002602;           % Molar mass of gas (g/mol)
+m_balloon = 500;            % Mass of the balloon (g)
+m_payload = 5000;           % Mass of the payload (g)
+dim_payload = [50 50 50];   % Size of the payload (cm)
